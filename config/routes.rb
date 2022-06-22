080c4afba9ce4e70b60bbfdc61962119653c2678
kaneko <kanekorei33@gmail.com>
2022-06-22 22:40:14 +0900

    users_controller,sessions_controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'users/new'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/comfirm'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
