9caccc2d91bf6b67cab40f4aaa889533bb15fba6
kaneko <kanekorei33@gmail.com>
2022-06-23 14:14:22 +0900

    プロフィール画像登録実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: %i[new create]
-  def new
+  
+  def index
+    @users = User.all
+  end
+
+  def
+     new
     @user = User.new
   end
 
@@ -18,12 +24,28 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def confirm
+    @user = current_user.users.build(user_params)
+    render :new if @user.invalid?
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if@user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+
   end
 
   private
   
   def user_params
-    params.require( :user).permit(:name, :email, :password, :password_confirmation)
+    params.require( :user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
   end
 end
 
