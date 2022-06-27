01a73f4dba3bad7fb2d950c042d026860169c027
kaneko <kanekorei33@gmail.com>
2022-06-27 10:23:06 +0900

    投稿が消せないエラーを解消

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,6 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-//import 'bootstrap';
+import 'bootstrap';
 import '../stylesheets/styles';
 import './scripts';
